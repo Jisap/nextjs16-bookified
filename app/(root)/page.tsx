1fcd4b015433d11f44714169fffaff70244b2f3f
Jorge Matos <jisapjisap@gmail.com>
2026-03-01 15:02:33 +0100

    feat: Implement initial homepage book display with `BookCard` components, define core application types, and configure Next.js image domains and server actions.

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,28 @@
+import BookCard from '@/components/Bookcard'
 import HeroSection from '@/components/HeroSection'
+import { sampleBooks } from '@/lib/constants'
+
+
+
 
 
 const page = () => {
   return (
-    <div className="wrapper container">
+    <main className="wrapper container pt-28 mb-10 md:mb-16">
       <HeroSection />
-    </div>
+
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
+    </main>
   )
 }
 
