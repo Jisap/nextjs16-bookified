4aace654dfd63a21959f150f54d3460c539aa947
Jorge Matos <jisapjisap@gmail.com>
2026-03-01 14:42:01 +0100

    feat: Add HeroSection component to the root page.

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import HeroSection from '@/components/HeroSection'
+
+
+const page = () => {
+  return (
+    <div className="wrapper container">
+      <HeroSection />
+    </div>
+  )
+}
+
+export default page
